3bd9f3ee0677d38c2b1287807ff7c4a0853f5731
wo-r <78428114+wo-r@users.noreply.github.com>
2024-01-24 14:45:27 -0600

    I'm changing the project to v2 for firefox addons
    contents of push:
    
    added a style injector (manages for @imports)
    converted manifest to firefox addon manifest

--- extension/script.js
+++ extension/script.js
@@ -1,6 +1,29 @@
 (async function () {
     "use strict";
-
     
+    $.ajax({
+        url: browser.extension.getURL('resource/stylesheet/main.css'),
+        method: 'get',
+        dataType: 'text',
+        success: async function(text) {
+            if ($("head #proview-css").length)
+                return;
 
+            await $.each(text.match(/@import\s+(?!url\(\s*['"]?https:\/\/[^'"\)]+['"]?\)\s*;)\s*(?:url\()?\s*['"]?([^'"\)]+)['"]?\)?[^;]*;/g), async function () {
+                $.ajax({
+                    url: browser.extension.getURL(`resource/stylesheet/${this.match(/@import\s+url\("([^"]+)"\);/, "")[1]}`),
+                    method: 'get',
+                    dataType: 'text',
+                    success: function (text) {
+                        $("head #proview-css").append(text.replace(/\/\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\//g, ""));
+                    }
+                });
+            })
+
+            text = text.replace(/@import\s+url\("((?!https?:\/\/).+)"\);/g, "");
+            text = text.replace(/\/\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\//g, "");
+
+            $("head").prepend(`<style id="proview-css">${text}</style>`);
+        }
+    });
 })();
--- extension/todo.md
+++
@@ -1,5 +0,0 @@
-### follow the todo strictly
-
-* if `base.json` exists, but localstorage item `config` does not, then create it using the base.json configuration. **(probably should just use a const variable called base_temp to do this on)**
-* add all the features in base.json for the functionality of script.js
-* do stylesheet (keep all images except for the sidemenu and avatars)
